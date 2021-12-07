c8fd26c10b6367f2d93ff1cbb8be734508c87cee
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2021-12-07 10:05:39 +0000

    clojure bootstrap

---
+++ advent-2021/test/advent_2021/day07_test.clj
@@ -0,0 +1,12 @@
+(ns advent-2021.core-test
+  (:require [clojure.test :refer :all]
+            [advent-2021.core :refer :all]))
+
+(deftest day-07
+  (testing
+    "part1"
+    (is (= 1 2))
+          )
+  )
+
+
